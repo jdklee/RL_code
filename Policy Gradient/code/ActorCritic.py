4083870cb620bc6d419a62ddb0cc0db7254c94b8
Jason Lee <jdk_lee@hotmail.com>
2022-08-07 22:41:48 -0400

    Added actor critic file that is independent of policy gradient py

--- Policy Gradient/code/ActorCritic.py
+++ Policy Gradient/code/ActorCritic.py
@@ -9,7 +9,6 @@ import gym
 import torch.nn as nn
 import os
 from general import get_logger, Progbar, export_plot
-from baseline_network import BaselineNetwork
 from network_utils import build_mlp, device, np2torch
 from policy import CategoricalPolicy, GaussianPolicy
 import matplotlib.pyplot as plt
@@ -27,7 +26,6 @@ class Critic(nn.Module):
         super().__init__()
         self.config = config
         self.env = env
-        self.baseline = None
         self.lr = self.config.learning_rate
 
 
@@ -45,34 +43,6 @@ class Critic(nn.Module):
         output=self.network(observations).squeeze()
         assert output.ndim == 1
         return output
-
-    def calculate_advantage(self, returns, observations):
-        """
-        Args:
-            returns: np.array of shape [batch size]
-                all discounted future returns for each step
-            observations: np.array of shape [batch size, dim(observation space)]
-        Returns:
-            advantages: np.array of shape [batch size]
-
-        TODO:
-        Evaluate the baseline and use the result to compute the advantages.
-        Put the advantages in a variable called "advantages" (which will be
-        returned).
-
-        Note:
-        The arguments and return value are numpy arrays. The np2torch function
-        converts numpy arrays to torch tensors. You will have to convert the
-        network output back to numpy, which can be done via the numpy() method.
-        """
-        observations = np2torch(observations)
-
-        values=self.forward(observations)
-
-        advantages = returns - values.detach().numpy()
-
-        return advantages
-
     def update_value(self, reward, new_observation, old_observation, I, done):
         """
         Args:
@@ -111,70 +81,28 @@ class Actor(nn.Module):
     Class for implementing Critic network that approximates the value of each state
     """
 
-    def __init__(self, env, config):
+    def __init__(self, env, config, lr=None, gamma=None):
 
         super().__init__()
         self.config = config
         self.env = env
-        self.baseline = None
         self.lr = self.config.learning_rate
 
-
-        #######################################################
-        #########   YOUR CODE HERE - 2-8 lines.   #############
         self.discrete = isinstance(env.action_space, gym.spaces.Discrete)
         self.observation_dim = self.env.observation_space.shape[0]
         self.action_dim = (
             self.env.action_space.n if self.discrete else self.env.action_space.shape[0]
         )
-        self.network =build_mlp(input_size=self.observation_dim, size=self.config.layer_size, n_layers=self.config.n_layers,
-                  output_size=self.action_dim)
-        self.optimizer = torch.optim.Adam(self.network.parameters() ,lr=self.lr)
+        # self.network =build_mlp(input_size=self.observation_dim, size=self.config.layer_size, n_layers=self.config.n_layers,
+        #           output_size=self.action_dim)
 
         if self.discrete:
             self.policy = CategoricalPolicy(self.network)
         else:
             self.policy = GaussianPolicy(self.network, action_dim=self.action_dim)
+        self.optimizer = torch.optim.Adam(self.policy.network.parameters(), lr=self.lr)
 
-
-        #######################################################
-        #########          END YOUR CODE.          ############
-
-
-
-    def forward(self, observations):
-        output=self.network(observations).squeeze()
-        assert output.ndim == 1
-        return output
-
-    def calculate_advantage(self, returns, observations):
-        """
-        Args:
-            returns: np.array of shape [batch size]
-                all discounted future returns for each step
-            observations: np.array of shape [batch size, dim(observation space)]
-        Returns:
-            advantages: np.array of shape [batch size]
-
-        TODO:
-        Evaluate the baseline and use the result to compute the advantages.
-        Put the advantages in a variable called "advantages" (which will be
-        returned).
-
-        Note:
-        The arguments and return value are numpy arrays. The np2torch function
-        converts numpy arrays to torch tensors. You will have to convert the
-        network output back to numpy, which can be done via the numpy() method.
-        """
-        observations = np2torch(observations)
-
-        values=self.forward(observations)
-
-        advantages = returns - values.detach().numpy()
-
-        return advantages
-
-
+        self.critic_network = Critic(self.env, self.config)
     def update_policy(self, reward, new_state, old_state, action, I, done):
         new_state = np2torch(new_state)
         old_state = np2torch(old_state)
@@ -185,24 +113,17 @@ class Actor(nn.Module):
         dist = self.policy.action_distribution(new_state)
         log_probabilties = dist.log_prob(action)
         if done:
-            advantage = reward - self.Critic(old_state)
+            advantage = reward - self.critic_network(old_state)
         else:
-            advantage = reward + self.config.gamma * self.Critic(new_state) - self.Critic(old_state)
+            advantage = reward + self.config.gamma * self.critic_network(new_state) - self.critic_network(old_state)
         self.loss = -torch.sum(log_probabilties * advantage) * I
         self.optimizer.zero_grad()
         self.loss.backward()
         self.optimizer.step()
 
     def train_actor_critic(self, num_episodes):
-        """
-        Performs training
-
-        """
-
         scores=[]
         recent_scores=deque(maxlen=100)
-
-
         for t in range(self.config.num_batches):
             env = self.env
             state = env.reset()
@@ -211,7 +132,9 @@ class Actor(nn.Module):
             score=0
 
             for step in range(self.config.max_ep_len):
+                # Pick action given state using policy
                 action = self.policy.act(state)[0]
+                # Observe new state, reward
                 new_state, reward, done, info = env.step(action)
                 # value = self.Critic(new_state)
                 # Q_SA = reward + self.config.gamma * self.Critic(new_state) if done else reward
@@ -219,7 +142,8 @@ class Actor(nn.Module):
                 score+=reward
 
                 # run training operations
-                self.Critic.update_value(reward=reward, new_observation=new_state, old_observation=state, I=I, done=done)
+                self.critic_network.update_value(reward=reward, new_observation=new_state, old_observation=state, I=I,
+                                                 done=done)
 
                 self.update_policy(reward=reward, new_state=new_state, old_state=state, I=I, action=action, done=done)
                 if done:
@@ -239,7 +163,7 @@ class Actor(nn.Module):
         plt.plot(scores)
         plt.ylabel('score')
         plt.xlabel('episodes')
-        plt.title('Training score of CartPole Actor-Critic TD(0)')
+        plt.title('Training score of {} Actor-Critic TD(0)'.format(self.env.name))
 
         reg = LinearRegression().fit(np.arange(len(scores)).reshape(-1, 1), np.array(scores).reshape(-1, 1))
         y_pred = reg.predict(np.arange(len(scores)).reshape(-1, 1))
